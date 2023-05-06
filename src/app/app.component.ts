31bffec775bb57eaa889bcb4fe477d779ec65423
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-05-07 00:46:25 +0100

    zip code search functionality implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'buscarCEP';
+
+  constructor(private http: HttpClient) {}
+
+  //variaveis do form
+  cep: string='';
+
+  //variaveis do retorno
+  endereco: any;
+  retorno={
+    rua:'',
+    cidade:'',
+    estado:''
+  }
+
+  //logradouro=rua, localidade=cidade, uf=estado
+  pesquisado = false;
+  espera = false;
+
+  pesquisa(){
+    this.espera = true;
+    this.cep = this.cep.replace(/\D/g,'');
+
+    const url = 'http://viacep.com.br/ws/' + this.cep + '/json/';
+
+    this.http.get(url).subscribe((res) => {
+      this.endereco = res;
+      this.retorno.rua = this.endereco.logradouro;
+      this.retorno.cidade = this.endereco.localidade;
+      this.retorno.estado = this.endereco.uf;
+    });
+
+    this.pesquisado = true;
+    this.espera = false;
+  }
+
+  limpar(){
+    this.pesquisado= false;
+    this.espera=false;
+    this.cep="";
+    this.endereco="";
+    this.retorno.rua="";
+    this.retorno.cidade="";
+    this.retorno.estado="";
+  }
+
 }
